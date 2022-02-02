d7327c6286307a639adf61bfc409090429416b80
Egon <loliegon@gmail.com>
2022-02-02 17:56:29 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -34,9 +34,46 @@ function App() {
         </div>
         <div className="bottom-main">
           <div className="asside">
-            <div className="product-groups"></div>
-            <div className="featured-list"></div>
-            <div className="contact-manager"></div>
+            <div className="product-groups">
+              <h2>Product Groups</h2>
+              <li>BAGS AND CASE</li>
+              <li>TABLE MAT</li>
+              <li>PLACEMATS/COASTER</li>
+              <li>PVC/PU LEATHER</li>
+            </div>
+            <div className="featured-list">
+              <h2>Feagured List</h2>
+              <li>Sample Available</li>
+              <li>Our Lifestyle</li>
+              <li>Recommended for you</li>
+              <li>Popular Products</li>
+              <li>Spot Goods</li>
+            </div>
+            <div className="contact-manager">
+              <h2>Contact manager</h2>
+              <div className="manager-cart">
+                <div className="manager-pic-name">
+                  <img
+                    className="manager-pic"
+                    src="/assets/account_circle_black_24dp.svg"
+                    alt=""
+                  />
+                  <p className="manager-name">Ben Affleck</p>
+                </div>
+
+                <textarea
+                  name="complaint"
+                  id="complaint"
+                  // @ts-ignore
+                  cols="33"
+                  // @ts-ignore
+                  rows="5"
+                ></textarea>
+                <div className="send-to-manager-button">
+                  <button className="send-button">SEND</button>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="products">
             <div className="product">
