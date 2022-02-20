fb00fa9e5b40aac5fb9367000104f31cf9715022
Egon <loliegon@gmail.com>
2022-02-20 13:19:00 +0100

    changes

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -4,21 +4,34 @@ export default function CartModal(props) {
   return (
     <div className="modal-wrapper">
       <div className="modal">
-        <button onClick={() => props.setModal(false)}>❌</button>
+        <div className="close-modal-button-wrapper">
+          <button id="close-modal-button" onClick={() => props.setModal(false)}>
+            <img src="../assets/close_white_24dp.svg" alt="" />
+          </button>
+        </div>
         <div id="modal">
           <div className="top-section">
             <div className="cart-image">
-              <img src="" alt="" />
+              <img
+                src="https://images.unsplash.com/photo-1573227896778-8f378c4029d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGVhdGhlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+                alt=""
+              />
             </div>
             <div className="cart-psp">
-              <p className="product-features">Product name</p>
-              <p className="product-features">
-                <span>Stock</span> : <span>3</span>
+              <p className="product-features" id="product-name">
+                Smooth Abrasion Resistant PVC Faux Leather Vinyl Leather Fabric
+                for Car Accessory Interior
               </p>
-              <p className="product-features">Price</p>
+              <p className="product-features" id="product-features-stock">
+                <span id="stock">Stock:</span>
+                <span id="stock-number">3</span>
+              </p>
+              <p className="product-features" id="price">
+                US$ 15/Meter
+              </p>
             </div>
           </div>
-          <div className="bottom section">
+          <div className="bottom-section">
             <div className="buy-button-wrapper">
               <button className="buy-button">Buy now!</button>
             </div>
