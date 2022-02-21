899412845d7a4d5233256aa9ecf5f1999ece7bcf
Egon <loliegon@gmail.com>
2022-02-21 19:47:22 +0100

    changes

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,3 +1,5 @@
+import "./Product.css";
+
 export default function Product() {
   return (
     <main className="main">
@@ -19,64 +21,228 @@ export default function Product() {
               US $1.5-2.5 / Meter | 500 Meters (Min. Order)
             </div>
             <div id="product-availability">
-              <div>
-                <p className="product-features">Thickness</p> : <p>0.6~1.2</p>
+              <div className="product-features">
+                <p className="key">Thickness:</p>
+                <p className="value">0.6~1.2</p>
               </div>
-              <div>
-                <p className="product-features">Usage</p> :{" "}
-                <p>
+              <div className="product-features">
+                <p className="key">Usage:</p>
+                <p className="value">
                   Bag, Shoes, Furniture, Decorative, Car Seat, Home Textile,
                   Gloves, Notebook, Belt
                 </p>
               </div>
-              <div>
-                <p className="product-features">Feature</p> :{" "}
-                <p>Abrasion Resistant, Soft</p>
+              <div className="product-features">
+                <p className="key">Feature:</p>
+                <p className="value">Abrasion Resistant, Soft</p>
               </div>
-              <div>
-                <p className="product-features">Width</p> : <p>54/55"</p>
+              <div className="product-features">
+                <p className="key">Width:</p>
+                <p className="value">54/55"</p>
               </div>
-              <div>
-                <p className="product-features">Pattern</p> : <p>Embossed</p>
+              <div className="product-features">
+                <p className="key">Pattern:</p>
+                <p className="value">Embossed</p>
               </div>
-              <div>
-                <p className="product-features">Material</p> : <p>PVC</p>
+              <div className="product-features">
+                <p className="key">Material:</p>
+                <p className="value">PVC</p>
               </div>
-              <div>
-                <p className="product-features">Samples</p> :{" "}
-                <p>US$ 15/Meter 1 Meter(Min.Order)</p>
+              <div className="product-features">
+                <p className="key">Samples:</p>
+                <p className="value">US$ 15/Meter 1 Meter(Min.Order)</p>
               </div>
-              <div>
-                <p className="product-features">Customization</p> :{" "}
-                <p>Available</p>
+              <div className="product-features">
+                <p className="key">Customization:</p>
+                <p className="value">Available</p>
               </div>
             </div>
           </div>
         </div>
+
+        <span id="product-dsc">Products Descriptions:</span>
         <div className="bottom-left">
-          <div className="product-descriptions"></div>
+          <div className="product-descriptions">
+            <div className="product-descriptions-left-tab">
+              <p className="left-tab-feature">Products Features: </p>
+              <p className="left-tab-feature">Material:</p>
+              <p className="left-tab-feature">MOQ:</p>
+              <p className="left-tab-feature">Technology:</p>
+              <p className="left-tab-feature">Customize Logo:</p>
+              <p className="left-tab-feature">Customize Size/shape</p>
+              <p className="left-tab-feature">Sample Charge:</p>
+              <p className="left-tab-feature">Sample time:</p>
+            </div>
+            <div className="product-descriptions-right-tab">
+              <p className="right-tab-info">Fashion and home decoration</p>
+              <p className="right-tab-info">Polyvinylchlorid</p>
+              <p className="right-tab-info">500 meters</p>
+              <p className="right-tab-info">embossed</p>
+              <p className="right-tab-info">
+                Print,debossed,and embroidery is welcome.
+              </p>
+              <p className="right-tab-info">
+                It can be made by any size and any shape.
+              </p>
+              <p className="right-tab-info" id="right-tab-info-price">
+                15 USD per set, 35-50 usd for DHL/FEDEX shipping cost(weight
+                around 1kg)
+              </p>
+              <p className="right-tab-info">3-5 working days</p>
+            </div>
+          </div>
           <div className="bottom-product-image">
-            <img id="bottom-product-image" src="" alt="" />
+            <img
+              id="bottom-product-image"
+              src="https://images.unsplash.com/photo-1573227896778-8f378c4029d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGVhdGhlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+              alt=""
+            />
           </div>
         </div>
       </div>
       <div className="right-main">
         <div className="top-right">
-          Contact menager
-          <div className="manager-pic-name">
-            <div id="manager-pic"></div>
-            <div id="manager-name"></div>
+          <div className="contact-manager">
+            <p>Contact manager</p>
           </div>
-          <div className="manager-input-send-button">
-            <div id="manager-input"></div>
-            <div id="manager-send-button"></div>
+          <div className="manager-pic-name">
+            <div id="manager-pic">
+              <img src="../assets/account_circle_black_24dp.svg" alt="" />
+            </div>
+            <div id="manager-name">Ben Affleck</div>
+          </div>
+          <div className="manager-send-button-wrapper">
+            <button id="manager-send-button">
+              <img src="../assets/email_white_24dp.svg" alt="" sizes="" />
+              <p>Contact now</p>
+            </button>
           </div>
         </div>
         <div className="bottom-right">
-          YOU MIGHT ALSO LIKE
-          <div className="bottom-right-product">
-            <div className="bottom-right-product-img"></div>
-            <div className="bottom-right-product-description"></div>
+          <div id="might-also-like-txt">
+            <p>YOU MIGHT ALSO LIKE:</p>
+          </div>
+          <div className="bottom-right-products">
+            <div className="bottom-right-product">
+              <div className="bottom-right-product-img">
+                <img
+                  src="https://images.unsplash.com/photo-1573227896778-8f378c4029d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGVhdGhlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+                  alt=""
+                />
+              </div>
+              <div className="bottom-right-product-description">
+                Smooth Abrasion Resistant PVC Faux Leather Vinyl Leather Fabric
+                for Car Accessory Interior
+              </div>
+            </div>
+            <div className="bottom-right-product">
+              <div className="bottom-right-product-img">
+                <img
+                  src="https://images.unsplash.com/photo-1573227896778-8f378c4029d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGVhdGhlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+                  alt=""
+                />
+              </div>
+              <div className="bottom-right-product-description">
+                Smooth Abrasion Resistant PVC Faux Leather Vinyl Leather Fabric
+                for Car Accessory Interior
+              </div>
+            </div>
+            <div className="bottom-right-product">
+              <div className="bottom-right-product-img">
+                <img
+                  src="https://images.unsplash.com/photo-1573227896778-8f378c4029d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGVhdGhlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+                  alt=""
+                />
+              </div>
+              <div className="bottom-right-product-description">
+                Smooth Abrasion Resistant PVC Faux Leather Vinyl Leather Fabric
+                for Car Accessory Interior
+              </div>
+            </div>
+            <div className="bottom-right-product">
+              <div className="bottom-right-product-img">
+                <img
+                  src="https://images.unsplash.com/photo-1573227896778-8f378c4029d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGVhdGhlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+                  alt=""
+                />
+              </div>
+              <div className="bottom-right-product-description">
+                Smooth Abrasion Resistant PVC Faux Leather Vinyl Leather Fabric
+                for Car Accessory Interior
+              </div>
+            </div>
+            <div className="bottom-right-product">
+              <div className="bottom-right-product-img">
+                <img
+                  src="https://images.unsplash.com/photo-1573227896778-8f378c4029d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGVhdGhlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+                  alt=""
+                />
+              </div>
+              <div className="bottom-right-product-description">
+                Smooth Abrasion Resistant PVC Faux Leather Vinyl Leather Fabric
+                for Car Accessory Interior
+              </div>
+            </div>
+            <div className="bottom-right-product">
+              <div className="bottom-right-product-img">
+                <img
+                  src="https://images.unsplash.com/photo-1573227896778-8f378c4029d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGVhdGhlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+                  alt=""
+                />
+              </div>
+              <div className="bottom-right-product-description">
+                Smooth Abrasion Resistant PVC Faux Leather Vinyl Leather Fabric
+                for Car Accessory Interior
+              </div>
+            </div>
+            <div className="bottom-right-product">
+              <div className="bottom-right-product-img">
+                <img
+                  src="https://images.unsplash.com/photo-1573227896778-8f378c4029d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGVhdGhlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+                  alt=""
+                />
+              </div>
+              <div className="bottom-right-product-description">
+                Smooth Abrasion Resistant PVC Faux Leather Vinyl Leather Fabric
+                for Car Accessory Interior
+              </div>
+            </div>
+            <div className="bottom-right-product">
+              <div className="bottom-right-product-img">
+                <img
+                  src="https://images.unsplash.com/photo-1573227896778-8f378c4029d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGVhdGhlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+                  alt=""
+                />
+              </div>
+              <div className="bottom-right-product-description">
+                Smooth Abrasion Resistant PVC Faux Leather Vinyl Leather Fabric
+                for Car Accessory Interior
+              </div>
+            </div>
+            <div className="bottom-right-product">
+              <div className="bottom-right-product-img">
+                <img
+                  src="https://images.unsplash.com/photo-1573227896778-8f378c4029d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGVhdGhlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+                  alt=""
+                />
+              </div>
+              <div className="bottom-right-product-description">
+                Smooth Abrasion Resistant PVC Faux Leather Vinyl Leather Fabric
+                for Car Accessory Interior
+              </div>
+            </div>
+            <div className="bottom-right-product">
+              <div className="bottom-right-product-img">
+                <img
+                  src="https://images.unsplash.com/photo-1573227896778-8f378c4029d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGVhdGhlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+                  alt=""
+                />
+              </div>
+              <div className="bottom-right-product-description">
+                Smooth Abrasion Resistant PVC Faux Leather Vinyl Leather Fabric
+                for Car Accessory Interior
+              </div>
+            </div>
           </div>
         </div>
       </div>
